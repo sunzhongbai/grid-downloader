a2f8695e4cb652edfe0562d950501c22ef2ac5eb
steinsun <sunzhongbai@foxmail.com>
2019-03-24 18:21:57 +0800

    grid-downloader.py

--- grid-downloader.py
+++ grid-downloader.py
@@ -5,7 +5,7 @@ import tarfile
 import shutil
 
 for i in range(1, 35):
-	url_auido = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}_50kHz.tar'
+	url_audio = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}_50kHz.tar'
 	#url = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}.tar'
 	print(f'Downlaod audio {str(i)} : ' + url_audio)
 	urllib.request.urlretrieve(url_audio)
@@ -23,17 +23,17 @@ for i in range(1, 35):
 	url_video1 = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/video/s{str(i)}.mpg_6000.part1.tar'
 	url_video2 = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/video/s{str(i)}.mpg_6000.part2.tar'
 
-	print(f'Downlaod audio {str(i)} : ' + url_audio)
+	print(f'Downlaod audio {str(i)} : ' + url_video1)
 	urllib.request.urlretrieve(url_video1)
 	urllib.request.urlretrieve(url_video2)
 	print('Start unzip audio {str(i)}')
 	with tarfile.open(f's{str(i)}.mpg_6000.part1.tar', 'r') as tar_video1:
-		print (tar.getmembers())
+		print (tar_video1.getmembers())
 		#input absolute path
 		tar_video1.extractall('/media/datas-2/steinsun/projects/grid-downloader/grid_video_tmp/')
 
 	with tarfile.open(f's{str(i)}.mpg_6000.part2.tar', 'r') as tar_video2:
-		print (tar.getmembers())
+		print (tar_video2.getmembers())
 		#input absolute path
 		tar_video2.extractall('/media/datas-2/steinsun/projects/grid-downloader/grid_video_tmp/')
 
