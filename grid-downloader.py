3c98271ffd196bef452f62513ad6cb4fb4fb60e7
szb <sunzhongbai@foxmail.com>
2019-03-24 14:12:07 +0800

    grid-downloader.py

--- grid-downloader.py
+++ grid-downloader.py
@@ -1,4 +1,23 @@
 import urllib
+import urllib.request
+from pathlib import Path
+import tarfile
+from shutil import copyfile
 
-url = "http://spandh.dcs.shef.ac.uk/gridcorpus/s1/audio/s1_50kHz.tar"
-urllib.urlretrieve(url,"nginx.tar")
+for i in range(1, 35):
+	url = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}_50kHz.tar'
+	#url = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}.tar'
+	print('Downlaod ' + url)
+	urllib.request.urlretrieve(url)
+	
+	print('Start unzip')
+	#with tarfile.open(f's{str(i)_50kHz.tar}', 'r') as tar:
+	with tarfile.open(f's{str(i)}.tar', 'r') as tar:
+		print (tar.getmembers())
+		#input absolute path
+		tar.extractall('/home/szb/datasets/grid_tmp')
+
+	#print('Move to dataset file')
+	#copyfile(f'./s{str(i)}/*', f'~/datasets/tmp2/s{str(i)}/audio/')
+	print(f'Done {str(i)} !')
+		
