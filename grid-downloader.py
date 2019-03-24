b588ce50f1bb6911597b6622fe26cc139dfb3088
steinsun <sunzhongbai@foxmail.com>
2019-03-24 18:59:51 +0800

    grid-downloader.py

--- grid-downloader.py
+++ grid-downloader.py
@@ -3,13 +3,13 @@ import urllib.request
 from pathlib import Path
 import tarfile
 import shutil
-import schedule
 
-for i in range(1, 35):
+for i in range(1, 21):
+
 	url_audio = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}_50kHz.tar'
 	#url = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}.tar'
 	print(f'Downlaod audio {str(i)} : ' + url_audio)
-	urllib.request.urlretrieve(url_audio, reportbook = Schedule)
+	urllib.request.urlretrieve(url_audio)
 	
 	print('Start unzip audio {str(i)}')
 	with tarfile.open(f's{str(i)}_50kHz.tar', 'r') as tar_audio:
@@ -25,8 +25,8 @@ for i in range(1, 35):
 	url_video2 = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/video/s{str(i)}.mpg_6000.part2.tar'
 
 	print(f'Downlaod audio {str(i)} : ' + url_video1)
-	urllib.request.urlretrieve(url_video1, reporthook = Schedule)
-	urllib.request.urlretrieve(url_video2, reporthook = Schedule)
+	urllib.request.urlretrieve(url_video1)
+	urllib.request.urlretrieve(url_video2)
 	print('Start unzip audio {str(i)}')
 	with tarfile.open(f's{str(i)}.mpg_6000.part1.tar', 'r') as tar_video1:
 		print (tar_video1.getmembers())
@@ -42,3 +42,41 @@ for i in range(1, 35):
 	shutil.copytree(f'/media/datas-2/steinsun/projects/grid-downloader/grid_video_tmp/s{str(i)}/video/mpg_6000/', f'/media/datas-2/steinsun/datasets/grid/s{str(i)}/video/')
 	print(f'Video {str(i)} Done !')
 		
+
+for i in range(22, 35):
+
+	url_audio = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}_50kHz.tar'
+	#url = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}.tar'
+	print(f'Downlaod audio {str(i)} : ' + url_audio)
+	urllib.request.urlretrieve(url_audio)
+	
+	print('Start unzip audio {str(i)}')
+	with tarfile.open(f's{str(i)}_50kHz.tar', 'r') as tar_audio:
+		print (tar_audio.getmembers())
+		#input absolute path
+		tar_audio.extractall('/media/datas-2/steinsun/projects/grid-downloader/grid_audio_tmp/')
+
+	print('Move to dataset file')
+	shutil.copytree(f'/media/datas-2/steinsun/projects/grid-downloader/grid_audio_tmp/s{str(i)}/', f'/media/datas-2/steinsun/datasets/grid/s{str(i)}/audio/')
+	print(f'Audio {str(i)} Done !')
+
+	url_video1 = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/video/s{str(i)}.mpg_6000.part1.tar'
+	url_video2 = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/video/s{str(i)}.mpg_6000.part2.tar'
+
+	print(f'Downlaod audio {str(i)} : ' + url_video1)
+	urllib.request.urlretrieve(url_video1)
+	urllib.request.urlretrieve(url_video2)
+	print('Start unzip audio {str(i)}')
+	with tarfile.open(f's{str(i)}.mpg_6000.part1.tar', 'r') as tar_video1:
+		print (tar_video1.getmembers())
+		#input absolute path
+		tar_video1.extractall('/media/datas-2/steinsun/projects/grid-downloader/grid_video_tmp/')
+
+	with tarfile.open(f's{str(i)}.mpg_6000.part2.tar', 'r') as tar_video2:
+		print (tar_video2.getmembers())
+		#input absolute path
+		tar_video2.extractall('/media/datas-2/steinsun/projects/grid-downloader/grid_video_tmp/')
+
+	#print('Move to dataset file')
+	shutil.copytree(f'/media/datas-2/steinsun/projects/grid-downloader/grid_video_tmp/s{str(i)}/video/mpg_6000/', f'/media/datas-2/steinsun/datasets/grid/s{str(i)}/video/')
+	print(f'Video {str(i)} Done !')
