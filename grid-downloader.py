acf680c4507cbf41b5d24b6f3dedcb4c47a5e18b
szb <sunzhongbai@foxmail.com>
2019-03-24 14:42:02 +0800

    grid-downloader.py

--- grid-downloader.py
+++ grid-downloader.py
@@ -5,19 +5,39 @@ import tarfile
 import shutil
 
 for i in range(1, 35):
-	url = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}_50kHz.tar'
+	url_auido = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}_50kHz.tar'
 	#url = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/audio/s{str(i)}.tar'
-	print('Downlaod ' + url)
-	urllib.request.urlretrieve(url)
+	print(f'Downlaod audio {str(i)} : ' + url_audio)
+	urllib.request.urlretrieve(url_audio)
 	
-	print('Start unzip')
-	#with tarfile.open(f's{str(i)_50kHz.tar}', 'r') as tar:
-	with tarfile.open(f's{str(i)}.tar', 'r') as tar:
+	print('Start unzip audio {str(i)}')
+	with tarfile.open(f's{str(i)_50kHz.tar}', 'r') as tar_audio:
+		print (tar_audio.getmembers())
+		#input absolute path
+		tar_audio.extractall('/home/szb/datasets/grid_tmp')
+
+	print('Move to dataset file')
+	shutil.copytree(f'./s{str(i)}/', f'/home/szb/datasets/tmp2/s{str(i)}/audio/')
+	print(f'Audio {str(i)} Done !')
+
+	url_video1 = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/video/s{str(i)}.mpg6000.part1.tar'
+	url_video2 = f'http://spandh.dcs.shef.ac.uk/gridcorpus/s{str(i)}/video/s{str(i)}.mpg6000.part2.tar'
+
+	print(f'Downlaod audio {str(i)} : ' + url_audio)
+	urllib.request.urlretrieve(url_video1)
+	urllib.request.urlretrieve(url_video2)
+	print('Start unzip audio {str(i)}')
+	with tarfile.open(f's{str(i)}.mpg6000.part1.tar', 'r') as tar_video1:
 		print (tar.getmembers())
 		#input absolute path
-		tar.extractall('/home/szb/datasets/grid_tmp')
+		tar_video1.extractall('/home/szb/datasets/grid_tmp')
+
+	with tarfile.open(f's{str(i)}.mpg6000.part2.tar', 'r') as tar_video2:
+		print (tar.getmembers())
+		#input absolute path
+		tar_video2.extractall('/home/szb/datasets/grid_tmp')
 
 	#print('Move to dataset file')
-	shutil.copytree(f'./s{str(i)}/', f'/home/szb/datasets/tmp2/s{str(i)}/audio/')
-	print(f'Done {str(i)} !')
+	shutil.copytree(f'.//s{str(i)}/', f'/home/szb/datasets/tmp2/s{str(i)}/video/')
+	print(f'Video {str(i)} Done !')
 		
